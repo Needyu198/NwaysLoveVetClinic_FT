b85eba91d0bb25278ef90619b369d73634c795a5
Needyu198 <htinaunglynn198@gmail.com>
2026-09-25 00:38:17 +0700

    HAL24

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2285800929" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "679289865" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
